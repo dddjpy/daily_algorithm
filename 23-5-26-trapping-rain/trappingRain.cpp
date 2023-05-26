a81369e07a74b76cd7214db62ef0fe0414e07366
dddjpy <jingpeiyang@eswincomputing.com>
2023-05-26 16:40:29 +0800

    2023.5.26.1
    Signed-off-by: dddjpy <jingpeiyang@eswincomputing.com>

--- 23-5-26-trapping-rain/trappingRain.cpp
+++ 23-5-26-trapping-rain/trappingRain.cpp
@@ -9,12 +9,14 @@
  */
 
 #include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
 class Solution {
     public:
-    // 遍历每一列，根据每一列左右最高最矮的柱子，求出所接的雨水，时间复杂度O{n²}，空间复杂度O(1);
+    // 方法一 ：暴力解法
+    // 遍历每一列，根据每一列左右最高最矮的柱子，求出所接的雨水，时间复杂度O(n²)，空间复杂度O(1);
     int violence (vector<int> &height) {
         int rain_sum = 0;
         // 第一根柱子和最后一根柱子肯定不会蓄水，所以不需要遍历；
@@ -30,18 +32,53 @@ class Solution {
                 right_max = std::max(height[j], right_max);
             }
             // 找出两端较矮的柱子，只有较矮的柱子高于当前柱子才会蓄水
-            int slide_max = std::min(left_max, right_max);
-            rain_sum = rain_sum + slide_max - height[i];
+            int slide_min = std::min(left_max, right_max);
+            if (slide_min > height[i])
+              rain_sum = rain_sum + slide_min - height[i];
         }
         return rain_sum;
     }
+
+    // 方法二 ：动态规划
+    // 暴力解法中时间复杂度可以降低，不用每次对每个柱子遍历，可以在一次遍历中求出每个柱子的左高和右高，这样时间和空间复杂度都是O(n)
+    int dynamic (vector<int> &height) {
+        int rain_sum = 0;
+        const int pillar_num = height.size();
+        // 首先用两个数组，max_left [i]代表第 i 列左边最高的墙的高度，max_right[i] 代表第 i 列右边最高的墙的高度。（一定要注意下，第 i 列左（右）边最高的墙，是不包括自身的，和力扣上边的讲的有些不同）
+        // 这种动态分配数组需要配合delete[] left_max进行释放，可以使用vector进行动态分配
+        // int* left_max = new int[pillar_num];
+        vector<int> left_max(pillar_num, 0);
+        vector<int> right_max(pillar_num, 0);
+
+        for (int i = 1; i < pillar_num - 1; i++) {
+            // 从左往右依次遍历柱子，计算当前柱子的左侧最高柱子，遍历不包括第0根柱子和第pillar_num-1（最后一根）柱子
+            left_max[i] = std::max(height[i - 1], left_max[i - 1]);
+        }
+
+        for (int i = pillar_num - 2; i > 0; i--) {
+            // 从右往左依次遍历柱子，计算当前柱子的右侧最高柱子，遍历不包括第0根柱子和第pillar_num-1（最后一根）柱子
+            right_max[i] = std::max(height[i + 1], right_max[i + 1]);
+        }
+
+        for (int i = 1; i < pillar_num - 1; i++) {
+            int pillar_min = std::min(left_max[i], right_max[i]);
+            if (pillar_min > height[i])
+              rain_sum = rain_sum + pillar_min - height[i];
+        }
+       return rain_sum;
+    }
+    // 方法三 ：双指针
+    // 方法四： 栈
 };
 
 int main() {
      vector<int> vec = {0,1,0,2,1,0,1,3,2,1,2,1};
-    
     Solution ss;
     int a = ss.violence (vec);
-    cout<<" ++++++ violence = "<<a<<endl; // ++++++ violence = 1
+    cout<<" ++++++ violence = "<<a<<endl; //  ++++++ violence = 6
+
+    vector<int> vec2 = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int b = ss.dynamic(vec2);
+    cout<<" ++++++ dynamic = "<<b<<endl; //  ++++++ dynamic = 6
     return 0;
 }
